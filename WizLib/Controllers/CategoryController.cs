833fcf3e1ccc8dcb6cb74abf776856b608d473b1
Carlos Clemente Zazueta Gonzalez <carlos.zazueta@unosquare.com>
2021-08-30 14:30:56 -0500

    Crud done with EF Core and Asp.Net

---
+++ WizLib/Controllers/CategoryController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WizLib_DataAccess.Data;
+using WizLib_Model.Models;
+
+namespace WizLib.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objList = _context.Categories.ToList();
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            var obj = new Category();
+
+            if (id == null)
+            {
+                return View(obj);
+            }
+
+            //For edit
+            obj = _context.Categories.FirstOrDefault(c => c.Category_Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Category_Id == 0)
+                {
+                    //Create
+                    _context.Categories.Add(obj);
+                }
+                else
+                {
+                    //Update
+                    _context.Categories.Update(obj);
+                }
+
+                _context.SaveChanges();
+
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(obj);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var obj = _context.Categories.FirstOrDefault(c => c.Category_Id == id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _context.Categories.Remove(obj);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
