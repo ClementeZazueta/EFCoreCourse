a999e3382b5cb861d2181b900b173bf4e7ff9bb3
Carlos Clemente Zazueta Gonzalez <carlos.zazueta@unosquare.com>
2021-08-31 12:45:02 -0500

    Finished EF Core section 7 Advanced Concepts

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using WizLib_DataAccess.Data;
 using WizLib_Model.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using WizLib_Model.Models;
 
 namespace WizLib.Controllers
 {
@@ -21,7 +22,12 @@ namespace WizLib.Controllers
         {
             var books = _context.Fluent_Books
                 .Include(b => b.Fluent_Publisher)
+                .Include(b => b.Fluent_BookAuthor)
+                .ThenInclude(ba => ba.Fluent_Author)
                 .ToList();
+
+            #region Explicit loading
+            //var books = _context.Fluent_Books.ToList();
             //foreach (var b in books)
             //{
             //    //Least efficient
@@ -29,7 +35,14 @@ namespace WizLib.Controllers
 
             //    //Explicit loading more efficient
             //    _context.Entry(b).Reference(p => p.Fluent_Publisher).Load();
+            //    _context.Entry(b).Collection(a => a.Fluent_BookAuthor).Load();
+
+            //    foreach (var bookAuth in b.Fluent_BookAuthor)
+            //    {
+            //        _context.Entry(bookAuth).Reference(ba => ba.Fluent_Author).Load();
+            //    }
             //}
+            #endregion
             return View(books);
         }
 
@@ -45,7 +58,6 @@ namespace WizLib.Controllers
             booksViewModel.Book = _context.Fluent_Books
                 .Include(b => b.Fluent_BookDetail)
                 .FirstOrDefault(a => a.Book_Id == id);
-            //booksViewModel.Book.Fluent_BookDetail = _context.Fluent_BookDetails
 
             if (booksViewModel == null)
             {
@@ -118,11 +130,117 @@ namespace WizLib.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult ManageAuthors(int id)
+        {
+            BookAuthorViewModel obj = new()
+            {
+                BookAuthorList = _context.Fluent_BookAuthors
+                    .Include(ba => ba.Fluent_Author)
+                    .Include(ba => ba.Fluent_Book)
+                    .Where(ba => ba.Book_Id == id)
+                    .ToList(),
+
+                BookAuthor = new Fluent_BookAuthor
+                {
+                    Book_Id = id
+                },
+
+                Book = _context.Fluent_Books.FirstOrDefault(b => b.Book_Id == id)
+            };
+
+            List<int> tempListOfAssignedAuthors = obj.BookAuthorList.Select(ba => ba.Author_Id).ToList();
+
+            //NOT IN clause in LINQ
+            var tempList = _context.Fluent_Authors.Where(a => !tempListOfAssignedAuthors.Contains(a.Author_Id)).ToList();
+
+            obj.AuthorList = tempList.Select(tl => new SelectListItem 
+            {
+                Text = tl.FullName,
+                Value = tl.Author_Id.ToString()
+            });
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult ManageAuthors(BookAuthorViewModel bookAuthorViewModel)
+        {
+            if (bookAuthorViewModel.BookAuthor.Book_Id != 0 && bookAuthorViewModel.BookAuthor.Author_Id != 0)
+            {
+                _context.Fluent_BookAuthors.Add(bookAuthorViewModel.BookAuthor);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookAuthorViewModel.BookAuthor.Book_Id });
+        }
+
+        [HttpPost]
+        public IActionResult RemoveAuthors(int authorId, BookAuthorViewModel bookAuthorViewModel)
+        {
+            int bookId = bookAuthorViewModel.Book.Book_Id;
+            Fluent_BookAuthor bookAuthor = _context.Fluent_BookAuthors
+                .FirstOrDefault(ba => ba.Author_Id == authorId && ba.Book_Id == bookId);
+
+            _context.Fluent_BookAuthors.Remove(bookAuthor);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(ManageAuthors), new { @id = bookId });
+        }
+
         public IActionResult Delete(int id)
         {
             var book = _context.Fluent_Books.FirstOrDefault(a => a.Book_Id == id);
 
             _context.Fluent_Books.Remove(book);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult PlayGround()
+        {
+            //var bookTemp = _context.Fluent_Books.FirstOrDefault();
+            //bookTemp.Price = 100;
+
+            //var bookCollection = _context.Fluent_Books;
+            //double totalPrice = 0.0;
+
+            //foreach (var book in bookCollection)
+            //{
+            //    totalPrice += book.Price;
+            //}
+
+            //var bookList = _context.Fluent_Books.ToList();
+            //foreach (var book in bookList)
+            //{
+            //    totalPrice += book.Price;
+            //}
+
+            //var bookCollection2 = _context.Fluent_Books;
+            //var bookCount1 = bookCollection2.Count();
+
+            //var bookCount2 = _context.Fluent_Books.Count();
+
+            IEnumerable<Fluent_Book> bookList1 = _context.Fluent_Books;
+            List<Fluent_Book> filteredBook1 = bookList1.Where(b => b.Price > 500).ToList();
+
+            IQueryable<Fluent_Book> bookList2 = _context.Fluent_Books;
+            List<Fluent_Book> filteredBook2 = bookList2.Where(b => b.Price > 500).ToList();
+
+            Fluent_Book bookTemp1 = _context.Fluent_Books
+                .Include(b => b.Fluent_BookDetail)
+                .FirstOrDefault(b => b.Book_Id == 3);
+            bookTemp1.Fluent_BookDetail.NumberOfChapters = 11;
+
+            _context.Fluent_Books.Update(bookTemp1);
+            _context.SaveChanges();
+
+            Fluent_Book bookTemp2 = _context.Fluent_Books
+                .Include(b => b.Fluent_BookDetail)
+                .FirstOrDefault(b => b.Book_Id == 3);
+            bookTemp2.Fluent_BookDetail.Weight = 1;
+
+            _context.Fluent_Books.Attach(bookTemp2);
             _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
---
+++ WizLib_Model/ViewModels/BookAuthorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizLib_Model.Models;
+
+namespace WizLib_Model.ViewModels
+{
+    public class BookAuthorViewModel
+    {
+        public Fluent_BookAuthor BookAuthor { get; set; }
+        public Fluent_Book Book { get; set; }
+        public IEnumerable<Fluent_BookAuthor> BookAuthorList { get; set; }
+        public IEnumerable<SelectListItem> AuthorList { get; set; }
+    }
+}
